887ce5e73bb1127d6993f2a4a3575b020f3bb99d
Elhan B Thomas <147646623+elhanbthomas@users.noreply.github.com>
2026-03-23 18:19:14 +0530

    R script

---
+++ job_analysis.R
@@ -0,0 +1,3 @@
+install.packages(c("tidyverse", "lubridate", "arules", "arulesViz",
+                   "wordcloud", "RColorBrewer", "ggplot2", "stringr",
+                   "tidytext", "topicmodels", "tm", "scales", "igraph"))
